00c9046edb2207371d8103512e1a6010537dc73f
Giovanni Miranda <biancheriadagio@gmail.com>
2026-03-18 00:41:48 +0100

    aggiunto env e gitignore

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabaseUrl = 'https://woifkjquaegnwsalslih.supabase.co'
-const supabaseAnonKey = 'sb_publishable_Eg0DfVXUTwVTiJnYWyPcyQ_RASZGD70'
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
